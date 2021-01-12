015f4ddac3cc810409a89a15905db33513e2dc21
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-01-12 03:07:27 +0000

    converted from Prototypes to ES6 classes

---
+++ appES6.js
@@ -0,0 +1,76 @@
+class Book {
+  constructor(author, title, isbn) {
+    this.author = author;
+    this.title = title;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  constructor() {}
+  addBookToList(book) {
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${book.author}</td>
+    <td>${book.title}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</td>
+    `;
+    list.appendChild(row);
+  }
+
+  // Delete Book
+  deleteBook(target) {
+    if (target.className === 'delete') {
+      if (confirm('Are You Sure')) {
+        target.parentElement.parentElement.remove();
+      }
+    }
+  }
+
+  // Clear Fields
+  clearFields() {
+    document.querySelector('#author').value = '';
+    document.querySelector('#title').value = '';
+    document.querySelector('#isbn').value = '';
+  }
+
+  // Show Alerts
+  showAlert(message, className) {
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    container.insertBefore(div, form);
+    setTimeout(() => {
+      document.querySelector('.alert').remove();
+    }, 2500);
+  }
+}
+
+document.querySelector('#book-form').addEventListener('submit', function(e) {
+  const author = document.querySelector('#author').value;
+  title = document.querySelector('#title').value;
+  isbn = document.querySelector('#isbn').value;
+
+  const book = new Book(author, title, isbn);
+
+  const bookUI = new UI();
+  if (title === '' || author === '' || isbn === '') {
+    bookUI.showAlert('Please Fill In All Fields', 'error');
+  } else {
+    bookUI.addBookToList(book);
+    bookUI.clearFields();
+    bookUI.showAlert('Book Successfully Added', 'success');
+  }
+  e.preventDefault();
+});
+
+document.querySelector('#book-list').addEventListener('click', function(e) {
+  const bookUI = new UI();
+  bookUI.deleteBook(e.target);
+  bookUI.showAlert('Book Deleted', 'delete');
+  e.preventDefault();
+});
